4f3752e1ec9ad955ce1ba23691b3f005aa5973ac
Sai Vamsi Krishna <velpulasaivamsikrishna@gmail.com>
2021-11-14 20:18:30 +0530

    first commit

---
+++ lib/src/main/java/HRGradleProject/Library.java
@@ -0,0 +1,7 @@
+package HRGradleProject;
+
+public class Library {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ lib/src/test/java/HRGradleProject/LibraryTest.java
@@ -0,0 +1,12 @@
+
+package HRGradleProject;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+    @Test void someLibraryMethodReturnsTrue() {
+        Library classUnderTest = new Library();
+        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+    }
+}
